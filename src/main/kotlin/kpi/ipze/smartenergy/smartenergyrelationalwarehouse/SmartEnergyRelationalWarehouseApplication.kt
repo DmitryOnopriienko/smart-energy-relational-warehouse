b5cf38a450359a80bd3a3cf8e54b8ea848ce47a3
dmytroonopriienko <dmitry.onopriienko@gmail.com>
2026-05-26 22:17:36 +0200

    Init

---
+++ src/main/kotlin/kpi/ipze/smartenergy/smartenergyrelationalwarehouse/SmartEnergyRelationalWarehouseApplication.kt
@@ -0,0 +1,11 @@
+package kpi.ipze.smartenergy.smartenergyrelationalwarehouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartEnergyRelationalWarehouseApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmartEnergyRelationalWarehouseApplication>(*args)
+}
